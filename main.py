ef99e5b4697de2e612b82c42f81567c09b142a2e
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2022-06-16 13:18:12 +0200

    let cloudscraper handle the `User-Agent` header

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import tenacity
 BASE_URL = 'https://www.blinkist.com/'
 
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0',
     'x-requested-with': 'XMLHttpRequest',
 }
 
